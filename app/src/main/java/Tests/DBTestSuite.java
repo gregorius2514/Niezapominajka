bb295117e1dc5d67d5fb00022bd6a7f291b8dc77
gregorius2514 <gregorius2514@wp.pl>
2014-07-30 22:59:39 +0200

    fixed DB write bugs. Add onFocus listeners to ET. Add autoincrement id for Tasks in DB.

---
+++ app/src/main/java/Tests/DBTestSuite.java
@@ -0,0 +1,7 @@
+package Tests;
+
+/**
+ * Created by grzesiek on 30.07.14.
+ */
+public class DBTestSuite {
+}
---
+++ app/src/main/java/pl/niezapominajka/app/AddTaskActivity.java
@@ -0,0 +1,7 @@
+package pl.niezapominajka.app;
+
+/**
+ * Created by grzesiek on 24.07.14.
+ */
+public class AddTaskActivity {
+}
