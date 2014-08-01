9c31ee249a469c00b2a718ce5febef7c43726ff0
gregorius2514 <gregorius2514@wp.pl>
2014-08-01 23:32:21 +0200

    Improve task's listView, Add onItemClickListener for listView, Fix many activites creaton on stack.

--- app/src/main/java/pl/niezapominajka/app/AddTaskActivity.java
+++ app/src/main/java/pl/niezapominajka/app/AddTaskActivity.java
@@ -1,7 +1,120 @@
 package pl.niezapominajka.app;
 
-/**
- * Created by grzesiek on 24.07.14.
- */
-public class AddTaskActivity {
-}
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+import database.SqliteController;
+import task.entity.Task;
+
+
+public class AddTaskActivity extends Activity {
+    // ------------ ATTRIBUTES
+    private Button btSave = null;
+    private DatePicker taskDatePicker = null;
+    private EditText etTaskName = null;
+    private EditText etTaskDesc = null;
+    private SqliteController dbController;
+
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.add_task);
+
+        // Objects and GUI elements
+        btSave = (Button) findViewById (R.id.btSaveTask);
+        taskDatePicker = (DatePicker) findViewById (R.id.taskDatePicker);
+        etTaskName = (EditText) findViewById (R.id.etTaskName);
+        etTaskDesc = (EditText) findViewById (R.id.etTaskDescription);
+        dbController = new SqliteController (getApplicationContext ());
+//        dbController = new SqliteController (this);
+
+        // listeners
+        btSave.setOnClickListener (new View.OnClickListener () {
+
+            @Override
+            public void onClick (View view) {
+                Task newTask = null;
+                int day, month, year;
+
+                day = taskDatePicker.getDayOfMonth ();
+                month = taskDatePicker.getMonth ();
+                year = taskDatePicker.getYear ();
+
+                newTask = new Task (0, etTaskName.getText ().toString ()
+                        , etTaskDesc.getText ().toString (), day, month, year);
+
+                if ( dbController.addTask (newTask) ) {
+//                    startActivity (new Intent (getApplicationContext (), pl.niezapominajka.app.MainActivity.class));
+                    finish();
+                } else {
+                    String errorMessage = "Error on write to Database.\nProbably invalid parameters";
+                    Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_LONG);
+                }
+            }
+        });
+
+        etTaskName.setOnFocusChangeListener (new FocusListener());
+        etTaskDesc.setOnFocusChangeListener (new FocusListener());
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater ().inflate (R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId ();
+        if  (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected (item);
+    }
+
+    class FocusListener implements View.OnFocusChangeListener {
+        private String oldText = null;
+
+        /*
+        metoda jest wywolywana gdy view wchodzi w stan focus oraz jest wywolywana
+        gdy opuszcza ten stan.
+
+        1. poprzedni tekst jest przechowywany i czyszczone pole tekstowe
+        2. podaczas utraty focus (i pusty tekst) zmienna oldText byla od nowa inicjalizowana
+        pusta wartoscia i to byl problem
+         */
+        @Override
+        public void onFocusChange (View view, boolean hasFocus) {
+            EditText et = (EditText) view;
+
+            if (!hasFocus ) {
+
+                if (et.getText().toString().length() == 0)
+                    et.setText (oldText);
+
+            } else {
+                oldText = et.getText().toString();
+                et.getText().clear();
+            }
+        }
+    }
+
+} // AddTaskActivity
--- app/src/main/java/pl/niezapominajka/app/MainActivity.java
+++ app/src/main/java/pl/niezapominajka/app/MainActivity.java
@@ -1,57 +1,74 @@
 package pl.niezapominajka.app;
 
-import android.content.DialogInterface;
-import android.graphics.BitmapFactory;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import database.SqliteController;
 import task.entity.Task;
 
-
 public class MainActivity extends ActionBarActivity {
     // ------------ ATTRIBUTES
-    private Button btDodaj = null;
-    private DatePicker taskDatePicker = null;
-    private SqliteController dbController;
-
+    private Button btAddTask = null;
+    private ListView lvTasksList = null;
+    private SqliteController dbController = null;
+    private  List<Task> tasksList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
-        btDodaj = (Button) findViewById(R.id.btSaveTask);
-        taskDatePicker = (DatePicker) findViewById(R.id.taskDatePicker);
-        // listeners
-        btDodaj.setOnClickListener(new ButtonListener());
 
-        dbController = new SqliteController(getApplicationContext());
+        // --- GUI elements
+        btAddTask = (Button) findViewById(R.id.btAddTask);
+        lvTasksList = (ListView) findViewById(R.id.listOfTasks);
+        dbController = new SqliteController(this);
 
-        // new Tasks for testing
-        Task t1 = new Task(7, "Dokonczyc projekt", "Aplikacja niezapominajka", 12, 12, 2014);
+        // --- Listeners
+        btAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent("pl.niezapominajka.app.ADDTASK"));
+//                finish();
+            }
+        });
 
-        Log.d("Create task", "task name: " + t1.getTaskName());
-        dbController.addTask(t1);
 
-        Log.d("Reading from db", "Read all tasks");
-        List<Task> tasks = dbController.getAllTasks();
 
-       for (Task t : tasks) {
-          String log = "Name:" + t.getTaskName() + " Desc:" + t.getTaskDesc()
-                  + " Day:" + t.getDay() + " Month:" + t.getMonth()
-                  + " Year:" + t.getYear();
-            Log.d("Task: ", log);
-        }
+        lvTasksList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Task task = tasksList.get(position);
 
+                String text = task.getDay() + "/" + task.getMonth() + "/" + task.getYear();
+
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+            }
+        });
+//TODO refactor/cleanup code
     }
 
 
@@ -75,19 +92,40 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    class ButtonListener implements Button.OnClickListener {
+    @Override
+    public void onResume() {
+        super.onResume();
+        // refresh listView
+        tasksList = dbController.getAllTasks();
 
-        @Override
-        public void onClick(View view) {
-            int day, month, year;
+        ArrayAdapter adapter = new TasksAdapter();
+        lvTasksList.setAdapter(adapter);
 
-            day = taskDatePicker.getDayOfMonth();
-            month = taskDatePicker.getMonth();
-            year = taskDatePicker.getYear();
-
-            System.out.println("Selected: \n\tDay: " + day +"\tMonth: " + month + "\tYear: " + year);
-        }
     }
 
+    private class TasksAdapter extends ArrayAdapter {
 
+        public TasksAdapter() {
+            super(MainActivity.this, R.layout.task_layout, tasksList);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemView = convertView;
+
+            if(itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.task_layout, parent, false);
+            }
+
+            Task currentTask = tasksList.get(position);
+
+            TextView title = (TextView) itemView.findViewById(R.id.title);
+            title.setText(currentTask.getTaskName());
+
+            TextView description = (TextView) itemView.findViewById(R.id.description);
+            description.setText(currentTask.getTaskDesc());
+
+            return itemView;
+        }
+    }
 }
